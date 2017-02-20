a301afd85d27976336bb8fd216e539d088fcf815
grapheneintelligentsystems <grapheneintelligentsystems@gmail.com>
2017-02-20 18:34:29 +0100

    added a POST action with JQuery-AJAX

--- JScripts/main.js
+++ JScripts/main.js
@@ -1,6 +1,13 @@
 $(function(){
   // $("body").append("<p> An other text</p>")
   var $orders = $('#orders');
+  var $name = $('#name');
+  var $drink = $('#drink');
+
+  function addOrderMB(order){
+    $orders.append('<li class="list">Name: '+order.name+', Drink: '+order.drink+', id:'+order.id+'</li>');
+  }
+
   $.ajax({
     type: 'GET',
     url: 'http://rest.learncode.academy/api/michail/orders/',
@@ -8,9 +15,31 @@ $(function(){
     success: function(orders){
       console.log('success', orders);
       $.each(orders, function(i, order){
-        $orders.append('<li class="list">Name: '+order.name+', Drink: '+order.drink+', id:'+order.id+'</li>');
+        addOrderMB(order);
       });
-    }
+    },
+    error: function(){
+      alert('error loading orders');
+    },
+  });
+
+  $('#add-order').on('click', function(){
+    var order = {
+      name: $name.val(),
+      drink: $drink.val(),
+    };
+
+    $.ajax({
+      type: 'POST',
+      url: 'http://rest.learncode.academy/api/michail/orders/',
+      data: order,
+      success: function(newOrder){
+        addOrderMB(newOrder);
+      },
+      error: function(){
+        alert('error saving orders');
+      },
+    });
   });
 
 });
